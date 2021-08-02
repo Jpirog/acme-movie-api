78e385181223a3e572e54084db6cc71f010e29fa
John Pirog <jpirog@hotmail.com>
2021-08-02 13:11:32 -0500

    added hello world http test

--- test/spec.js
+++ test/spec.js
@@ -1,33 +1,21 @@
 const app = require('supertest')(require('../app'));
-const { expect } = require('chai')
-const request = require('supertest')
+const { expect } = require('chai');
+//const request = require('supertest');
+const { syncAndSeed } = require('../db');
 
-describe('first', () => {
-  it('it1', () => {
-    // no test,just to show the test software is working
+describe('Basic tests', () => {
+  beforeEach(()=> syncAndSeed());
+  describe('ping test', () => {
+    it('it1', () => {
+      // no test,just to show the test software is working
+    })
+  })
+  describe('test for hello world', () => {
+    it('shows Hello World', async () => {
+      const response = await app.get('/');
+      expect (response.status).to.equal(200);
+      expect (response.text).to.equal('Hello World')
+    })
   })
 })
 
-// describe('second', () => {
-//   it('shows Hello World', async () => {
-//     const response = await app.get('/');
-//     console.log(response)
-//     expect (response.status).to.equal(200);
-//     expect (response.text).to.equal('Hello World')
-//   })
-// })
-//syncAndSeed();
-describe('second', () => {
-   it('shows Hello World', async () => {
-// request(app)
-//   .get('/')
-//   .expect(200)
-//   .then((response) => {
-//     console.log(response.body)
-//     expect(response.body.text.toBe('Hello World'))
-  // })
-  // .end((err,res) => {
-  //   if (err) throw err;
-  // })
-})
-})
